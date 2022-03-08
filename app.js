e2b61a32f888b66e5f37ec7e4df06e75296268f8
Roberto Paulino <roberto.a.paulino001@gmail.com>
2022-03-08 14:56:32 -0500

    added function to randomly choose the computer choice, created function with logic that determines the winner of the round

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('im working dood')
+function computerPlay () {
+    let choice = 3;
+
+    choice = getRandomInt(3);
+
+    if (choice == 2) {
+        return 'rock';
+    } else if (choice == 1) {
+        return 'paper';
+    } else if (choice == 0) {
+        return 'scissors';
+    } else {
+        console.log ('something went wrong with the computerPlay functtion.')
+    }
+
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function playRound(computerSelection, playerSelection) {
+    let result = '';
+    playerSelection.toLowerCase();
+    
+    if ((computerSelection == 'rock' && playerSelection == 'scissors' ) || (computerSelection == 'paper' && playerSelection == 'rock') || (computerSelection == 'scissors' && playerSelection == 'paper')) {
+        result = `You lose! ${computerSelection} beats ${playerSelection}.`
+    } else if ((computerSelection == 'scissors' && playerSelection == 'rock' ) || (computerSelection == 'rock' && playerSelection == 'paper') || (computerSelection == 'paper' && playerSelection == 'scissors')) {
+        result = `You win! ${playerSelection} beats ${computerSelection}.`
+    } else {
+        result = "It's a tie!"
+    }
+    
+    return result;
+}
+
+// function game(){
+
+//     for (let i = 0; i < 5; i++) {
+        
+//     }
+
+// }
